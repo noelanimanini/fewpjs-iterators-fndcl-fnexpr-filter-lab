a3e172722dfa707f620e8c370ea6acb14e6fbbcd
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-18 15:24:47 -0800

    it wont resolve the last function but i know im right? help

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+let findMatching = (drivers, string) => {
+   return drivers.filter(names => names.toLowerCase() === string.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, string) => {
+    return drivers.filter(names => names.match() === string.match())
+}
+
+let matchName = (drivers, string) => {
+    return drivers.filter(record => record.name === string)
+}
+
+// function matchName(drivers, string){
+//     return drivers.filter(driver => driver.name === string)
+// }
